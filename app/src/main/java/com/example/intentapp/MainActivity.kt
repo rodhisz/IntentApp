efa76c1218dcc2a3f295e9290d8fef15afe30e17
Rodhi Shafia Zaidan <rodhisz.45@gmail.com>
2023-09-05 00:13:45 +0700

    [Add Feature] Move Activity With Data

--- app/src/main/java/com/example/intentapp/MainActivity.kt
+++ app/src/main/java/com/example/intentapp/MainActivity.kt
@@ -11,15 +11,30 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //1. Move Activity
         val btnMoveActivity : Button = findViewById(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener(this)
+
+        //2. Move Activity With Data
+        val btnMoveWithDataActivity : Button = findViewById(R.id.btn_move_with_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
+
+            //1. Move Activity
             R.id.btn_move_activity -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            //2. Move Activity With Data
+            R.id.btn_move_with_data -> {
+                val moveData = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveData.putExtra(MoveWithDataActivity.EXTRA_NAME, "Rodhi")
+                moveData.putExtra(MoveWithDataActivity.EXTRA_AGE, 23)
+                startActivity(moveData)
             }
         }
     }
